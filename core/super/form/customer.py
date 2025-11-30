b766c9dcd9581a8252b22aae2227f4ffd045c6ab
barquetbravokadir@gmail.com <barquetbravokadir@gmail.com>
2025-11-30 16:58:17 -0500

    Formulario de clientes con validaciones de los inputs

--- core/super/form/customer.py
+++ core/super/form/customer.py
@@ -8,15 +8,15 @@ class CustomerForm(ModelForm):
         fields = ['name', 'last_name', 'dni', 'email', 'address', 'phone', 'birth_date', 'gender']
         widgets = {
             'dni': forms.TextInput(attrs={
-                'class': 'form-control only-numbers', # Clase clave para el JS
+                'class': 'form-control only-numbers', # Clase controlada por JS
                 'placeholder': 'Cédula (10 dígitos)',
-                'maxlength': '10',  # Límite estricto de caracteres
+                'maxlength': '10',  # Límite estricto
                 'minlength': '10',
                 'pattern': '[0-9]{10}',
                 'autocomplete': 'off'
             }),
             'phone': forms.TextInput(attrs={
-                'class': 'form-control only-numbers',
+                'class': 'form-control only-numbers', # Clase controlada por JS
                 'placeholder': 'Teléfono (10 dígitos)',
                 'maxlength': '10',  # Límite estricto
                 'minlength': '10',
