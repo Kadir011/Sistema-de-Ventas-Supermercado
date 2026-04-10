9f7ad199214f741be6fd9a9c24ed0dddbd0229a8
Kadir011 <barquetbravokadir@gmail.com>
2026-04-10 08:40:34 -0500

    Optimización de la configuración de la base de datos implementando SSL para Neon

--- config/settings.py
+++ config/settings.py
@@ -94,14 +94,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
 
 DATABASES = {
     "default": {
@@ -111,7 +103,10 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD", ""),
         'HOST': os.environ.get("DB_SOCKET", ""),
         'PORT': os.environ.get("DB_PORT", "5432"),
-        'ATOMIC_REQUESTS': True
+        'ATOMIC_REQUESTS': True,
+        'OPTIONS': {
+            'sslmode': 'require', # <--- ESTO ES VITAL PARA NEON
+        }
     }
 }
 
